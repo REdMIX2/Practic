2508fb4e0dde03ef56d088ab7d6fd95d7bb7b063
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-03 14:48:34 +0300

    Update jupyter_tests.py

--- jupyter_tests.py
+++ jupyter_tests.py
@@ -66,13 +66,14 @@ def test_values():
         from matplotlib_assignment import a,b,c,d,x,y
     except ImportError:
         pass
-    global score,CNT_Values
+    global score
+    global CNT_Values
     tmp = globals()
     #from test_image import CNT_Values
     if CNT_Values >= 1 :
         assert 'a' in tmp,f"ddd" 
         assert ( (type(tmp['a']) == int or float ),f"Problems a 'a'"
-                CNT_Values -= 1
+        CNT_Values = CNT_Values - 1
         if CNT_Values >= 1 :
             assert (( 'b' in tmp ) and (type(tmp['b']) == int or float)),f"Problems a 'b'"
             CNT_Values -= 1
