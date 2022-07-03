beb49e0eb7c557c0b8ceff3e1afd7329430683a6
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-03 17:42:03 +0300

    Update jupyter_tests.py

--- jupyter_tests.py
+++ jupyter_tests.py
@@ -101,7 +101,7 @@ def test_value():
     del param[CNT_Values:4]
     for t in param:
         assert exec("from matplotlib_assignment import "+str(t))==None,f"Create a variable '{t}'!Don't forget to initialize it"
-        if (t not "x") and (t not "y"):
+        if (t != "x") and (t != "y"):
             assert (isinstance(t,(int, float)),f"The variable '{t}' is not an int or float!"
         else:
             assert (isinstance(t,(list, tuple, np.array)),f"The variable '{t}' is not an array!"
