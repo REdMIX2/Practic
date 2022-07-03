df87bf40fada555d35bedc34fe369dcd8790c829
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-03 16:57:01 +0300

    Update jupyter_tests.py

--- jupyter_tests.py
+++ jupyter_tests.py
@@ -91,12 +91,16 @@ def test_task_id():
    # score += 1
 def test_value():
     global score
-    for t in (["a","b","c","d","x","y"]):
+    param=["a","b","c","d","x","y"]
+    for t in param:
         try:
             exec("from matplotlib_assignment import "+str(t))
         except ImportError:
             print("ggggggg=")
             print(t)
+    del param[CNT_Values:3]
+    for t in param:
+        assert exec("from matplotlib_assignment import "+str(t)),f"Create and initialize the variable{t}!"
     score += 1
         
 #def test_create_images():
