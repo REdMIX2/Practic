8ef07736653091040baccb40c3ea8fdf2b3cf545
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-04 23:57:10 +0300

    Update Add_img_13.py

--- Add_img_13.py
+++ Add_img_13.py
@@ -1,10 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import pytest
-a=1+1
+a=1
 b=2
 c=3
-x = np.arange(0, 2*np.math.pi, 0.1)
+x = np.arange(0, 20, 0.1)
 from matplotlib_assignment import my_function,my_plot
 y=my_function(x,a,b,c)
 @pytest.mark.mpl_image_compare(baseline_dir='baseline',
