f284e7b32474410d1aa692b3f065043784b92705
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-04 23:23:06 +0300

    Update Create_img_3.py

--- Create_img_3.py
+++ Create_img_3.py
@@ -1,15 +1,14 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import pytest
+a=1
+b=2
+c=3
+x = np.arange(0, 2*np.math.pi, 0.1)
 from matplotlib_assignment import my_function,my_plot
-
+y=my_function(x,a,b,c)
 @pytest.mark.mpl_image_compare(baseline_dir='baseline',
                                filename='other_name1.png',tolerance=3)
 def test_succeeds():
-  a=1
-  b=2
-  c=3
-  x = np.arange(0, 2*np.math.pi, 0.1)
-  y=my_function(x,a,b,c)
   my_plot()
   return plt.gca().get_figure()
