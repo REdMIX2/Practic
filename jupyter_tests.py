f7ee819b324ac20d912f71482f6d5510aab2135f
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-04 22:44:05 +0300

    Update jupyter_tests.py

--- jupyter_tests.py
+++ jupyter_tests.py
@@ -79,7 +79,8 @@ def test_create_images():
     if task_id is not None:
         #c=subprocess.call("pytest --mpl-generate-path=baseline Create_img_"+str(CNT_Values)+".py", shell=True)
         c=subprocess.run("pytest --mpl-generate-path=baseline Create_img_"+str(CNT_Values)+".py", shell=True)
-        assert c.returncode==4,"Image not generate!"
+        print(c.CalledProcessError)
+        assert c.returncode==2,"Image not generate!"
         
 def test_maths():
     if Student_ID is not None:
