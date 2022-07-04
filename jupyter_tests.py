f72cd0d443a7f9fa7b59ea173e607b554296a3bc
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-04 22:46:32 +0300

    Update jupyter_tests.py

--- jupyter_tests.py
+++ jupyter_tests.py
@@ -79,7 +79,7 @@ def test_create_images():
     if task_id is not None:
         #c=subprocess.call("pytest --mpl-generate-path=baseline Create_img_"+str(CNT_Values)+".py", shell=True)
         c=subprocess.run("pytest --mpl-generate-path=baseline Create_img_"+str(CNT_Values)+".py", shell=True)
-        print(c.CalledProcessError)
+        print(c.stderr)
         assert c.returncode==2,"Image not generate!"
         
 def test_maths():
