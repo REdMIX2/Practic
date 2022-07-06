a94f9cd0adb4a1388c36e7bcd6ce0bcc8591fa6f
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-06 14:19:47 +0300

    Update Create_img_3.py

--- Create_img_3.py
+++ Create_img_3.py
@@ -1,12 +1,22 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import pytest
+from jupyter_tests import CNT_argv
+from matplotlib_assignment import my_function,my_plot,task_id
 a=1
 b=2
 c=3
+d=2
 x = np.arange(0, 2*np.math.pi, 0.1)
-from matplotlib_assignment import my_function,my_plot
-y=my_function(x,a,b,c)
+match CNT_argv[task_id]:
+  case 1:
+    y=my_function(x)
+  case 2:
+    y=my_function(x,a)
+  case 3:
+    y=my_function(x,a,b)
+  case 4:
+    y=my_function(x,a,b,c,d)
 @pytest.mark.mpl_image_compare(filename='other_name1.png',tolerance=3)
 def test_succeeds():
   my_plot()
