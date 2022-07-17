ad5fc88ce1952a4749fb716fb6369e8c4b4ceb0a
REdMIX2 <99914085+REdMIX2@users.noreply.github.com>
2022-07-17 21:56:25 +0300

    Update jupyter_tests.py

--- jupyter_tests.py
+++ jupyter_tests.py
@@ -95,8 +95,7 @@ def test_formula():
 def test_images():
     global score
     c=subprocess.run("pytest --mpl test_img.py", shell=True)
-    tmp =c.stdout.decode('utf-8')
-    print(tmp)
+    print(c.stdout)
     assert c.returncode==0,"Incorrect image!"
     score += 1
     
